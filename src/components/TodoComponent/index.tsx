8f1d6c0cc1fd306ee220bd6884d5430a71476fbb
Bibek Dhungana <Dhunganabibek2016@gmail.com>
2022-09-29 18:12:27 -0500

    slightly UI modified

--- src/components/TodoComponent/index.tsx
+++ src/components/TodoComponent/index.tsx
@@ -1,41 +1,32 @@
-import { Box, Flex, Heading, Icon, IconButton } from '@chakra-ui/react';
-import { BsFillAlarmFill } from 'react-icons/bs';
+import { Box, Flex, Heading, Icon, IconButton, Spacer } from '@chakra-ui/react';
+import { BsFillSunFill } from 'react-icons/bs';
 import TodoInput from '../TodoInput';
 import TodoList from '../TodoList';
 
 function TodoComponent() {
   return (
     <Flex
-      as="main"
       w="100%"
-      h="400px"
+      h="35vh"
       bgImage="assets/bgImage.jpg"
-      position="absolute"
       bgSize="cover"
       bgPosition="center"
       direction="column"
+      align="center"
     >
-      <Flex
-        direction="column"
-        maxW="600px"
-        position="relative"
-        mx="auto"
-        top="50"
-        p="5"
-      >
-        <Flex justify="space-between">
-          <Heading as="h1" fontSize="50px">
-            TODO
-          </Heading>
-          <IconButton
-            aria-label="toggle mode"
-            colorScheme="transparent"
-            icon={<Icon as={BsFillAlarmFill} />}
-            size="lg"
-          />
-        </Flex>
+      <Flex w="24%" justifyContent="space-between" mt="80px" mb="20px">
+        <Heading as="h1" fontSize="50px">
+          TODO
+        </Heading>
+        <IconButton
+          aria-label="toggle mode"
+          colorScheme="transparent"
+          icon={<Icon as={BsFillSunFill} />}
+          fontSize="30px"
+          color="white"
+        />
       </Flex>
-      <Box mt="50px">
+      <Box>
         <TodoInput />
         <TodoList />
         {/* TODO: Create FilterTodo Component */}
